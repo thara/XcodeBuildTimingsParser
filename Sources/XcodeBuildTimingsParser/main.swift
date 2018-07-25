0baff878077afb3648972bc5b169681a7dbd7995
Tomochika Hara <git@thara.jp>
2018-07-26 00:11:46 +0900

    Open file by --file option

--- Sources/XcodeBuildTimingsParser/main.swift
+++ Sources/XcodeBuildTimingsParser/main.swift
@@ -2,7 +2,16 @@ import Commander
 import Swiftline
 
 let main = command { (filename:String) in
-  print("Reading file \(filename)".f.Red)
+    print("Reading file \(filename)".f.Red)
+
+    if let text = try? String(contentsOfFile: filename, encoding: String.Encoding.utf8) {
+        var lines: [String] = []
+        text.enumerateLines { line, _ in 
+            lines.append(line)
+        }
+        print(lines)
+    }
+
 }
 
 main.run()
