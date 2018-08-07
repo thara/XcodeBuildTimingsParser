46a799c88f925396d231dbfe990fd402d7ecb9ff
Tomochika Hara <git@thara.jp>
2018-08-07 23:31:19 +0900

    Cleanup old functions

--- Sources/XcodeBuildTimingsParser/Parser.swift
+++ Sources/XcodeBuildTimingsParser/Parser.swift
@@ -1,24 +1,5 @@
-// Example
-// 000000 | TIME:     3.20r     5.06u     1.34s      31 /      0   CompileSwiftSources normal armv7 com.apple.xcode.tools.swift.compiler
-
-// LINE ::= NO " | TIME: " VALUE "r " VALUE "u " VALUE "s " PAGE " / " PAGE " " COMMAND
-// NO = 8DIGIT
-// VALUE = 1*DIGIT
-// PAGE = 1*DIGIT
-// OMMAND = 1*(ALPHA|DIGIT|SP|"/")
-// 
 
 struct RecordParser {
-
-    let input: String
-    var pos = 0
-    var readPos = 0
-    var ch: Character? = nil
-
-    init(input: String) {
-        self.input = input
-        readChar()
-    }
 
     static func parse(input: String) -> Record? {
         let line = Array(input)
@@ -59,26 +40,6 @@ struct RecordParser {
         (cmd, pos) = parseCommandString(input: line, pos: pos)
 
         return Record(no: no!, real: real!, user: user!, sys: sys!, pageIn: pageIn!, pageOut: pageOut!, commandString: cmd)
-    }
-
-    mutating func readChar() {
-        if input.count <= readPos {
-            ch = nil
-        } else {
-            let i = input.index(input.startIndex, offsetBy: readPos)
-            ch = input[i]
-        }
-        pos = readPos
-        readPos += 1
-    }
-
-    func peekChar() -> Character? {
-        if input.count <= readPos {
-            return nil
-        } else {
-            let i = input.index(input.startIndex, offsetBy: readPos)
-            return input[i]
-        }
     }
 
     static func isDigit(_ c: Character) -> Bool {
--- Tests/XcodeBuildTimingsParserTest/ParserSpec.swift
+++ Tests/XcodeBuildTimingsParserTest/ParserSpec.swift
@@ -6,63 +6,6 @@ import Quick
 class ParserSpec: QuickSpec {
     override func spec() {
 
-        describe("its readChar") {
-            context("when input is empty") {
-                let parser = RecordParser(input: "")
-                it("points to 0 index on input string") {
-                    expect(parser.pos).to(equal(0))
-                }
-                it("points to 1 index for reading position") {
-                    expect(parser.readPos).to(equal(1))
-                }
-                it("has not read character from input string") {
-                    expect(parser.ch).to(beNil())
-                }
-            }
-            context("when input is not empty") {
-                let parser = RecordParser(input: "abcdefg")
-                it("points to 0 index on input string") {
-                    expect(parser.pos).to(equal(0))
-                }
-                it("points to 1 index for reading position") {
-                    expect(parser.readPos).to(equal(1))
-                }
-                it("has read the first character from input string") {
-                    expect(parser.ch).to(equal("a"))
-                }
-
-                context("after readChar is 1 called") {
-                    var parser = RecordParser(input: "abcdefg")
-                    parser.readChar()
-                    it("points to 1 index on input string") {
-                        expect(parser.pos).to(equal(1))
-                    }
-                    it("points to 2 index for reading position") {
-                        expect(parser.readPos).to(equal(2))
-                    }
-                    it("has read the 2nd character from input string") {
-                        expect(parser.ch).to(equal("b"))
-                    }
-                }
-
-                context("after readChar is 2 called") {
-                    var parser = RecordParser(input: "abcdefg")
-                    parser.readChar()
-                    parser.readChar()
-
-                    it("points to 2 index on input string") {
-                        expect(parser.pos).to(equal(2))
-                    }
-                    it("points to 3 index for reading position") {
-                        expect(parser.readPos).to(equal(3))
-                    }
-                    it("has read the 3rd character from input string") {
-                        expect(parser.ch).to(equal("c"))
-                    }
-                }
-            }
-        }
-
         describe("its isDigit") {
             context("when a digit character is passed") {
                 for n: Character in ["0", "9"] {
