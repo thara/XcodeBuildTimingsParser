f35a3b3bec79d318e39cd2445e4c90a7d75cef9b
Tomochika Hara <git@thara.jp>
2018-08-11 22:31:18 +0900

    Add --sort / --reverse option

--- Sources/XcodeBuildTimingsParser/Models.swift
+++ Sources/XcodeBuildTimingsParser/Models.swift
@@ -8,6 +8,8 @@ public struct Record : CustomStringConvertible {
     let pageOut: Int
     let commandString: String
 
+    public static let fields = ["No", "Real", "User", "Sys", "PageIn", "PageOut", "CommandString"]
+
     init(no: Int, real: Double, user: Double, sys: Double, pageIn: Int, pageOut: Int, commandString: String) {
         self.no = no
         self.real = real
@@ -20,5 +22,26 @@ public struct Record : CustomStringConvertible {
 
     public var description: String {
         return "\(no),\(real),\(user),\(sys),\(pageIn),\(pageOut),\(commandString)"
+    }
+
+    public static func compared(by field: String) -> (Record, Record) -> Bool {
+        return {
+            switch field {
+                case "No":
+                    return $0.no < $1.no
+                case "Real":
+                    return $0.real < $1.real
+                case "User":
+                    return $0.user < $1.user
+                case "Sys":
+                    return $0.sys < $1.sys
+                case "PageIn":
+                    return $0.pageIn < $1.pageIn
+                case "PageOut":
+                    return $0.pageOut < $1.pageOut
+                default:
+                    return false
+            }
+        }
     }
 }
--- Sources/XcodeBuildTimingsParserMain/main.swift
+++ Sources/XcodeBuildTimingsParserMain/main.swift
@@ -14,29 +14,47 @@ struct StandardErrorOutputStream: TextOutputStream {
     }
 }
 
+let recordFields = Record.fields.joined(separator: ",")
+
+func readRecordLines(_ filename: String) -> ArraySlice<String>? {
+    guard let text = try? String(contentsOfFile: filename, encoding: String.Encoding.utf8) else {
+        return nil
+    }
+
+    var lines: [String] = []
+    text.enumerateLines { line, _ in 
+        lines.append(line)
+    }
+
+    // remove headers & summary
+    return lines.suffix(from: 2).prefix(while: { !$0.hasPrefix(" ") } )
+}
+
 Group {
-    $0.command("csv",
-        Argument<String>("filename")
-    ) { (filename) in
-        guard let text = try? String(contentsOfFile: filename, encoding: String.Encoding.utf8) else {
+    $0.command("dump",
+        Argument<String>("filename"),
+        Option("sort", default: "No", description: "Sort column"),
+        Flag("reverse", description: "Reverse sort in order")
+    ) { (filename, sortField, reverse) in
+
+        guard Record.fields.contains(sortField) else {
+            var errStream = StandardErrorOutputStream()
+            print("Invalid field name : '\(sortField)'".f.Red, to: &errStream)
+            print("Choose one field from {\(recordFields)}".f.Red, to: &errStream)
+            exit(EXIT_FAILURE)
+        }
+
+        guard let recordLines = readRecordLines(filename) else {
             var errStream = StandardErrorOutputStream()
             print("Can not open \(filename).".f.Red, to: &errStream)
             exit(EXIT_FAILURE)
         }
 
-        var lines: [String] = []
-        text.enumerateLines { line, _ in 
-            lines.append(line)
-        }
+        print(recordFields)
 
-        // remove headers & summary
-        let details = lines.suffix(from: 2).prefix(while: { !$0.hasPrefix(" ") } )
+        let cmp = Record.compared(by: sortField)
+        let recordComparator = reverse ? { !cmp($0, $1) } : cmp
 
-        print("No,Real,User,Sys,PageIn,PageOut,CommandString")
-        details.forEach { line in
-            if let record = RecordParser.parse(input: line) {
-                print("\(record)")
-            }
-        }
+        recordLines.map { RecordParser.parse(input: $0) }.compactMap { $0 }.sorted(by: recordComparator).forEach { r in print("\(r)") }
     }
 }.run()
